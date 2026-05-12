1272bbcf9ac7d4ed0eeee98e3c5949f712c43727
Richy-2003 <rchadryan@gmail.com>
2026-05-11 22:21:51 -0300

    adiciona endpoint para cadastro de usuario

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,6 +6,22 @@ function getAllUsers(request, response) {
    return response.json(users)  
 }
 
+function createUser(req, res)  {
+       // const name = req.body.name
+
+        const {name} = req.body
+
+        const newUser = {
+            id: Date.now(),
+            name: name
+        }
+
+    const createdUser = userModel.create(newUser)
+
+    return res.status(201).json(createdUser)
+}
+
 module.exports = {
+    createUser,
     getAllUsers
 }
